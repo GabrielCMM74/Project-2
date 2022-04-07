33d192e718b70343dd86a6a2239ed888ba2af884
GabrielCMM74 <97140818+GabrielCMM74@users.noreply.github.com>
2022-04-07 16:32:24 -0400

    First commit for boilerplate

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+// replace your database connection string here
+mongoose.connect(process.env.DATABASE_URL,{ 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+const db = mongoose.connection;
+
+// database connection event
+db.on('connected', function () {
+  console.log(`Mongoose connected to: ${db.host}:${db.port}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var router = require('express').Router();
+const passport = require('passport');
+
+// The root route renders our only view
+router.get('/', function(req, res) {
+  // Where do you want to go for the root route
+  // in the student demo this was res.redirect('/students'), what do you want?
+  // This could be a landing page, or just redirect to your main resource page which you'll have an a tag that makes 
+  // a request to `/auth/google` route below
+});
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect : '/', // where do you want the client to go after you login 
+    failureRedirect : '/' // where do you want the client to go if login fails
+  }
+));
+
+// OAuth logout route
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
