8ea2c5f431548fb0b4ff81aba089061fea3f1b1f
GabrielCMM74 <97140818+GabrielCMM74@users.noreply.github.com>
2022-04-08 20:23:16 -0400

    Add error page for 404

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
@@ -6,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 const methodOverride = require('method-override');
-const indexRoutes = require('./routes/index');
+
 // load the env consts
 require('dotenv').config();
 
@@ -15,9 +16,8 @@ const app = express();
 
 // connect to the MongoDB with mongoose
 require('./config/database');
-// configure Passport
 require('./config/passport');
-
+const indexRoutes = require('./routes/index');
 
 
 // view engine setup
@@ -52,9 +52,22 @@ app.use(function (req, res, next) {
 app.use('/', indexRoutes);
 
 
+
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+
 // invalid request, send 404 page
-app.use(function(req, res) {
-  res.status(404).send('Cant find that!');
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
