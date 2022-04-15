50a1d96d8854e36fae3da081bca3907c65ee20b7
GabrielCMM74 <97140818+GabrielCMM74@users.noreply.github.com>
2022-04-15 02:23:49 -0400

    Finished except for Stocks chart

--- controllers/portfolios.js
+++ controllers/portfolios.js
@@ -1,7 +1,6 @@
 const portfolio = require("../models/portfolio");
 const Portfolio = require("../models/portfolio");
 
-
 module.exports = {
     index,
     create,
@@ -12,18 +11,14 @@ module.exports = {
     error
   };
 
-
 function index(req, res){
-    console.log(req.user, '<<<<<req user ')
     Portfolio.find({}, function (err, portfolios) {
       res.render('portfolios/index', { portfolios, title: "Portfolio Center" });
   });
 }
 function error(req, res){
       res.render('error', {title: "Error Page" });
-  
 }
-
 function create(req, res){
   req.body.user = req.user._id;
   req.body.userName = req.user.name;
@@ -32,8 +27,6 @@ function create(req, res){
       res.redirect('portfolios/new');
   })
 }
-
-
 function newPortoflio(req, res) {
   const newPortfolio = new Portfolio();
   Portfolio.find({}, function (err, portfolios) {
@@ -43,47 +36,18 @@ function newPortoflio(req, res) {
 function deletePortfolio(req, res) {
   Portfolio.findByIdAndDelete(req.params.id, function(err, portfolios){
     if (err) return res.redirect('/');
-      console.log(portfolios);
       portfolios.save(function(err){
         res.redirect('/portfolios');
       })
     });
   };
-
-
 function detailPortfolio(req, res) {
   Portfolio.findById(req.params.id, function (err, detailsPortfolio) {
     if (err) return res.redirect('/portfolios/index');
     res.render('portfolios/edit', {detailsPortfolio, title: "Edit Portfolio" })
   })}
-
-
 function editPortfolio(req, res) {
   Portfolio.findByIdAndUpdate(req.params.id, req.body, {new:true}, function (err, overridePortfolio) {
     if (err) return res.redirect('/portfolios/index');
-    // console.log(req.body, '<--- Req body')
-    // console.log(overridePortfolio, '<--- override')
       res.redirect(`/portfolios/${req.params.id}`)
   })}
-
-
-
-
-
-
-
-// function show(req, res, next) {
-//   Portfolio.findById(req.params.id, function (err, flight) {
-//     if (err) return res.redirect('/flights');{
-//       res.render('flights/show', { flight, tickets, title: 'Flight Detail' });
-//     }})}
-
-
-
-// function newFlight(req, res) {
-//   const newFlight = new Flight();
-//   departDefault = newFlight.departs;
-//   const departsDate = departDefault.toISOString().slice(0, 16);
-//   res.render("flights/new", { departsDate, title: "Add Flight" });
-// }
-
